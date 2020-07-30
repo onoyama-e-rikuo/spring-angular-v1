cebda76e06234a13c1941193a3e34421690d0f78
Onoyama Rikuo <evang00dd@gmail.com>
2020-07-30 18:08:12 +0900

    新規ユーザー登録機能追加

--- src/main/java/net/matondo/resource/UserResource.java
+++ src/main/java/net/matondo/resource/UserResource.java
@@ -1,15 +1,30 @@
 package net.matondo.resource;
 
 import net.matondo.domain.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.matondo.exception.ExceptionHandling;
+import net.matondo.exception.domain.EmailExistException;
+import net.matondo.exception.domain.UserNotFoundException;
+import net.matondo.exception.domain.UsernameExistException;
+import net.matondo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/user")
-public class UserResource {
-    @GetMapping("/home")
-    public String  showUser(){
-        return "application works";
+@RequestMapping(path = {"/", "/user"})
+public class UserResource extends ExceptionHandling {
+    private UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<User>  register(@RequestBody User user) throws UserNotFoundException, UsernameExistException, EmailExistException {
+        User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(),user.getEmail());
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
 }
--- src/main/java/net/matondo/service/UserService.java
+++ src/main/java/net/matondo/service/UserService.java
@@ -2,12 +2,15 @@ package net.matondo.service;
 
 
 import net.matondo.domain.User;
+import net.matondo.exception.domain.EmailExistException;
+import net.matondo.exception.domain.UserNotFoundException;
+import net.matondo.exception.domain.UsernameExistException;
 
 import javax.mail.MessagingException;
 import java.util.List;
 
 public interface UserService  {
-    User register(String firstName, String lastName, String username, String email);
+    User register(String firstName, String lastName, String username, String email) throws UserNotFoundException, UsernameExistException, EmailExistException;
 
     List<User> getUsers();
 
